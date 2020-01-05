3a1a79fa8af8f9ba1b27171c7949cf967e257add
Ecochoupipig <1315943577@qq.com>
2020-01-05 16:02:41 +0800

    建立config-server项目

---
+++ airnet/service-zuul/src/main/java/com/zsc/servicezuul/ServiceZuulApplication.java
@@ -0,0 +1,19 @@
+package com.zsc.servicezuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
+@EnableZuulProxy //开启路由网关功能
+public class ServiceZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceZuulApplication.class, args);
+    }
+
+}
