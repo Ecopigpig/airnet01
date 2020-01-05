fe8f4631c99ee8faa1664bda5910720f7debd84f
Ecochoupipig <1315943577@qq.com>
2020-01-05 16:49:45 +0800

    分布式配置中心(Spring Cloud Config)(Finchley版本),注意是bootstrap不要拼写错误！

--- airnetConfig/config-client/src/test/java/com/zsc/configclient/ConfigClientApplicationTests.java
+++ airnetConfig/config-client/src/test/java/com/zsc/configclient/ConfigClientApplicationTests.java
@@ -1,6 +1,6 @@
 package com.zsc.configclient;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
--- airnetConfig/config-server/src/test/java/com/zsc/configserver/ConfigServerApplicationTests.java
+++ airnetConfig/config-server/src/test/java/com/zsc/configserver/ConfigServerApplicationTests.java
@@ -1,6 +1,6 @@
 package com.zsc.configserver;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
