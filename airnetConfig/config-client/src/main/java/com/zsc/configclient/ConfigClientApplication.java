82b1f3b598ed343967a7b3f0708ab5bd60b5876a
Ecochoupipig <1315943577@qq.com>
2020-01-11 17:25:16 +0800

    消息总线(Spring Cloud Bus)(Finchley版本)

--- airnetConfig/config-client/src/main/java/com/zsc/configclient/ConfigClientApplication.java
+++ airnetConfig/config-client/src/main/java/com/zsc/configclient/ConfigClientApplication.java
@@ -3,6 +3,8 @@ package com.zsc.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @EnableEurekaClient
+@EnableDiscoveryClient
+@RefreshScope
 public class ConfigClientApplication {
+
+    /**
+     * 用postman用post请求以下网址即可动态获取到git上的配置信息
+     * 要注意的是要设置：headers:Content-Type:Application/json
+     * http://localhost:8881/actuator/refresh
+     */
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
     }
 
+    //打开网址访问：http://localhost:8881/hi，网页显示：
+    //foo version 3
     @Value("${foo}")
     String foo;
     @RequestMapping(value = "/hi")
