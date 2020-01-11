702a88b6c7beba52c5372b071839cb0afd0f8ef2
Ecochoupipig <1315943577@qq.com>
2020-01-11 23:20:52 +0800

    服务链路追踪待解决,网页上看不到调用过程(Spring Cloud Sleuth)(Finchley版本)

--- airnet/service-miya/src/main/java/com/zsc/servicemiya/ServiceMiyaApplication.java
+++ airnet/service-miya/src/main/java/com/zsc/servicemiya/ServiceMiyaApplication.java
@@ -1,14 +1,12 @@
-package com.zsc.servicehi;
+package com.zsc.servicemiya;
 
 import brave.sampler.Sampler;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,13 +16,29 @@ import java.util.logging.Logger;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
-public class ServiceHiApplication {
+public class ServiceMiyaApplication {
+
+    //要cmd启动zipkin-server才能对服务链路进行追踪
+    //java -jar zipkin-server-2.10.1-exec.jar
+    //然后访问地址http://localhost:9411/zipkin/
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceHiApplication.class, args);
+        SpringApplication.run(ServiceMiyaApplication.class, args);
     }
 
-    private static final Logger LOG = Logger.getLogger(ServiceHiApplication.class.getName());
+    private static final Logger LOG = Logger.getLogger(ServiceMiyaApplication.class.getName());
+
+    @RequestMapping("/hi")
+    public String home(){
+        LOG.log(Level.INFO, "hi is being called");
+        return "hi i'm miya!";
+    }
+
+    @RequestMapping("/miya")
+    public String info(){
+        LOG.log(Level.INFO, "info is being called");
+        return restTemplate.getForObject("http://localhost:8763/info",String.class);
+    }
 
     @Autowired
     private RestTemplate restTemplate;
@@ -34,27 +48,10 @@ public class ServiceHiApplication {
         return new RestTemplate();
     }
 
-    @Value("${server.port}")
-    String port;
-
-    @RequestMapping("/hi01")
-    public String home(@RequestParam(value = "name", defaultValue = "zsc") String name) {
-        return "hi " + name + " ,i am from port:" + port;
-    }
-
-    @RequestMapping("/hi")
-    public String callHome(){
-        LOG.log(Level.INFO, "calling trace service-hi  ");
-        return restTemplate.getForObject("http://localhost:8989/miya", String.class);
-    }
-    @RequestMapping("/info")
-    public String info(){
-        LOG.log(Level.INFO, "calling trace service-hi ");
-        return "i'm service-hi";
-    }
 
     @Bean
     public Sampler defaultSampler() {
         return Sampler.ALWAYS_SAMPLE;
     }
+
 }
