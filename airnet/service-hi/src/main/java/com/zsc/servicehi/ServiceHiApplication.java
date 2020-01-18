b6f642b4773f757baf13557414c43b64eaf010ea
Ecochoupipig <1315943577@qq.com>
2020-01-18 16:40:07 +0800

    只闪现看到了一个监控页面,待解决,断路器监控(Hystrix Dashboard)(Finchley版本),

--- airnet/service-hi/src/main/java/com/zsc/servicehi/ServiceHiApplication.java
+++ airnet/service-hi/src/main/java/com/zsc/servicehi/ServiceHiApplication.java
@@ -1,12 +1,18 @@
 package com.zsc.servicehi;
 
 import brave.sampler.Sampler;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +26,20 @@ import java.util.logging.Logger;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
+@EnableHystrix  //开启断路器
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class ServiceHiApplication {
+
+    /**
+     * 访问下面地址能看见白熊：
+     * http://localhost:8763/hystrix
+     * 先访问一下这个服务,再访问断路器地址,才能看见数据：
+     * http://localhost:8763/hi
+     * 断路器访问地址 ：
+     * http://localhost:8763/actuator/hystrix.stream
+     * @param args
+     */
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceHiApplication.class, args);
@@ -37,15 +56,30 @@ public class ServiceHiApplication {
         return new RestTemplate();
     }
 
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+
     @Value("${server.port}")
     String port;
 
-    @RequestMapping("/hi01")
+    @RequestMapping("/hi")
+    @HystrixCommand(fallbackMethod = "hiError")
     public String home(@RequestParam(value = "name", defaultValue = "zsc") String name) {
         return "hi " + name + " ,i am from port:" + port;
     }
 
-    @RequestMapping("/hi")
+    public String hiError(String name){
+        return "hi,"+name+",sorry,error!";
+    }
+
+    @RequestMapping("/hiTrack")
     public String callHome(){
         LOG.log(Level.INFO, "calling trace service-hi  ");
         return restTemplate.getForObject("http://localhost:8989/miya", String.class);
